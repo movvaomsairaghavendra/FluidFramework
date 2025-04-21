c833be6e51f5662370acc4aa938a23bed1987ee2
Daniel Madrid <danielcar@microsoft.com>
2025-04-21 11:12:35 -0700

    webpack trusted types in docusaurus config

--- docs/src/theme/Root.tsx
+++ docs/src/theme/Root.tsx
@@ -3,9 +3,30 @@
  * Licensed under the MIT License.
  */
 
-import React from "react";
+import React, { useEffect } from "react";
 
 export type RootProps = React.PropsWithChildren;
+
+declare global {
+  interface TrustedTypePolicy {
+    createHTML(input: string): string;
+    createScript(input: string): string;
+    createScriptURL(input: string): string;
+  }
+
+  interface TrustedTypePolicyFactory {
+    createPolicy(name: string, rules: {
+      createHTML?: (input: string) => string;
+      createScript?: (input: string) => string;
+      createScriptURL?: (input: string) => string;
+    }): TrustedTypePolicy;
+    getPolicy(name: string): TrustedTypePolicy | undefined;
+  }
+
+  interface Window {
+    trustedTypes?: TrustedTypePolicyFactory;
+  }
+}
 
 /**
  * Root component of Docusaurus's React tree.
@@ -14,5 +35,27 @@ export type RootProps = React.PropsWithChildren;
  * @see {@link https://docusaurus.io/docs/swizzling#wrapper-your-site-with-root}
  */
 export default function Root({ children }: RootProps): React.ReactElement {
+	useEffect(() => {
+	const tt = (window as any).trustedTypes;
+
+    if (
+      tt &&
+      typeof tt.createPolicy === 'function' &&
+      typeof tt.getPolicy === 'function' &&
+      !tt.getPolicy('ff#webpack')
+    ) {
+	  // Create a policy to allow the use of trusted types in the application
+	  tt.createPolicy('ff#webpack', {
+        createHTML: (input: any) => input,
+        createScript: (input: any) => input,
+        createScriptURL: (input: any) => input,
+      });
+    } else {
+		console.warn(
+			"Trusted Types are not supported in this browser. Please consider using a browser that supports Trusted Types for better security."
+		);
+	}
+  }, []);
+
 	return <>{children}</>;
 }
