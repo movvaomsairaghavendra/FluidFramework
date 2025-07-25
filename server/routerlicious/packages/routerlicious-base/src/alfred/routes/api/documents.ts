7eb9877d031b0cb135b9c6fb42b33457340f9f04
Xin Zhang <zhangxin@microsoft.com>
2025-07-25 13:56:58 -0400

    Make create doc overwrittable

--- server/routerlicious/packages/routerlicious-base/src/alfred/routes/api/documents.ts
+++ server/routerlicious/packages/routerlicious-base/src/alfred/routes/api/documents.ts
@@ -205,7 +205,6 @@ export function create(
 		"alfred:sessionCacheTTLForDeletedDocumentsSec",
 	);
 
-	const hybridCreationEnabled: boolean = config.get("alfred:hybridCreationEnabled") ?? false;
 	const ignoreEphemeralFlag: boolean = config.get("alfred:ignoreEphemeralFlag") ?? true;
 	// Whether to enforce server-generated document ids in create doc flow
 	const enforceServerGeneratedDocumentId: boolean =
@@ -382,7 +381,6 @@ export function create(
 				values,
 				enableDiscovery,
 				isEphemeral,
-				hybridCreationEnabled,
 				messageBrokerId,
 			);
 
